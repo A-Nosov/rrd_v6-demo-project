4747fa25eaaea4cbe8315fdeb0071350a2a25e4f
Anton Nosov <nosov.anton1994@gmail.com>
2022-11-13 20:32:38 +0300

    rrd_v6

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { isLoggedInSelector } from '../store/authSlice'
+function ProtectedRoute({ redirectTo = '/auth/login', children }) {
+    const location = useLocation()
+    const isLoggedIn = useSelector(isLoggedInSelector())
+    if (!isLoggedIn)
+        return <Navigate to={redirectTo} state={{ referrer: location }} />
+    return children
+}
+export default ProtectedRoute
